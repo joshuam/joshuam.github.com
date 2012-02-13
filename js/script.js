26acd934d209840db852781ed2ef35b000f7bf17
Josh McCarthy <mccarthy.josh@gmail.com>
2012-02-13 17:41:37 -0330

    Add some content to the site

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-/* Author: 
-
-*/
+$(document).ready(function(){
 
 
-
-
-
+    function hide()
+    {
+        $('.nav').each(function(){
+            $(this).addClass('hide');
+        });
+    }
+    
+    function toggle(link, div)
+    {
+        $(link).click(function(){
+            hide();
+            $(div).removeClass('hide');
+        });
+    }
+    
+    toggle('#navAbout', '#about');
+    toggle('#navCode', '#code');
+    toggle('#navConnect', '#connect');
+});
 
 
 
