00863367747ec806f490c1f1bdcf549990e9b73a
Josh McCarthy <mccarthy.josh@gmail.com>
2012-04-17 21:47:22 -0400

    Change header color

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
     ctx.fillStyle = 'rgba(38,91,243, 1.0)';
+    ctx.fillStyle = 'rgba(241,243,217, 1.0)';
     ctx.font='80px Ubuntu Mono, monospace';
 
     function hide()
